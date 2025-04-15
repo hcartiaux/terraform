57fc48ab8b981988e16a857df67df5717a43d975
Hyacinthe Cartiaux <hyacinthe.cartiaux@free.fr>
2025-04-15 17:42:33 +0200

    [scaleway] New nodes pl-war1 and fr-par1

--- scaleway/main.tf
+++ scaleway/main.tf
@@ -39,3 +39,47 @@ resource "scaleway_domain_record" "ns2" {
   data     = "ns2.flap42.eu."
   ttl      = 1800
 }
+
+###
+# Instances
+###
+
+# dn42 node pl-war1
+resource "scaleway_instance_ip" "pl_war1_ipv6" {
+  type = "routed_ipv6"
+  zone = "pl-waw-2"
+}
+resource "scaleway_instance_server" "pl-war1" {
+  name = "pl-war1.flap42.eu"
+  zone = "pl-waw-2"
+  type = "STARDUST1-S"
+  image = "debian_bookworm"
+
+  # No IPv4 configuration
+  enable_dynamic_ip = false
+
+  # Attach the IPv6 address
+  ip_id = scaleway_instance_ip.pl_war1_ipv6.id
+
+  tags = [ "dn42" ]
+}
+
+# dn42 node fr-par1
+resource "scaleway_instance_ip" "fr_par1_ipv6" {
+  type = "routed_ipv6"
+  zone = "fr-par-1"
+}
+resource "scaleway_instance_server" "fr-par1" {
+  name = "fr-par1.flap42.eu"
+  zone = "fr-par-1"
+  type = "STARDUST1-S"
+  image = "debian_bookworm"
+
+  # No IPv4 configuration
+  enable_dynamic_ip = false
+
+  # Attach the IPv6 address
+  ip_id = scaleway_instance_ip.fr_par1_ipv6.id
+
+  tags = [ "dn42" ]
+}
