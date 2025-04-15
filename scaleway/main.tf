7b2c5573876658312ca36c9846124d4cd464af74
Hyacinthe Cartiaux <hyacinthe.cartiaux@free.fr>
2025-04-15 18:17:57 +0200

    [scaleway] Reverse names

--- scaleway/main.tf
+++ scaleway/main.tf
@@ -55,12 +55,17 @@ resource "scaleway_domain_record" "ns2" {
 
 # dn42 node pl-war1
 resource "scaleway_instance_ip" "pl_war1_ipv6" {
-  type = "routed_ipv6"
   zone = "pl-waw-2"
+  type = "routed_ipv6"
+}
+resource "scaleway_instance_ip_reverse_dns" "pl_war1_ipv6_reverse" {
+  zone = "pl-waw-2"
+  ip_id = scaleway_instance_ip.pl_war1_ipv6.id
+  reverse = "pl-war1.flap42.eu."
 }
 resource "scaleway_instance_server" "pl-war1" {
-  name = "pl-war1.flap42.eu"
   zone = "pl-waw-2"
+  name = "pl-war1.flap42.eu"
   type = "STARDUST1-S"
   image = "debian_bookworm"
 
@@ -75,12 +80,17 @@ resource "scaleway_instance_server" "pl-war1" {
 
 # dn42 node fr-par1
 resource "scaleway_instance_ip" "fr_par1_ipv6" {
-  type = "routed_ipv6"
   zone = "fr-par-1"
+  type = "routed_ipv6"
+}
+resource "scaleway_instance_ip_reverse_dns" "fr_par1_ipv6_reverse" {
+  zone = "fr-par-1"
+  ip_id = scaleway_instance_ip.fr_par1_ipv6.id
+  reverse = "fr-par1.flap42.eu."
 }
 resource "scaleway_instance_server" "fr-par1" {
-  name = "fr-par1.flap42.eu"
   zone = "fr-par-1"
+  name = "fr-par1.flap42.eu"
   type = "STARDUST1-S"
   image = "debian_bookworm"
 
