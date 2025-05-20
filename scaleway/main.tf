334d0fce5c255d6d9ca7bc6b3c399e33e636fdec
Hyacinthe Cartiaux <hyacinthe.cartiaux@free.fr>
2025-05-20 23:05:04 +0200

    [scaleway] Also update the account ssh public key

--- scaleway/main.tf
+++ scaleway/main.tf
@@ -4,7 +4,7 @@
 
 resource "scaleway_account_ssh_key" "main" {
     name       = "main"
-    public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICtAyNLxu6GqPOLQutasE70tPMOSF+WS9LmK6kldYwPD hcartiaux@hc-xps13"
+    public_key = var.ssh_pub_key
 }
 
 ###
