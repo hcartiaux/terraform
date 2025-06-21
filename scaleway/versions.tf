8c5c77eae9461f5cc291fb9c95f30050f2950bf6
Hyacinthe Cartiaux <hyacinthe.cartiaux@free.fr>
2025-06-22 01:45:33 +0200

    Lint!

--- scaleway/versions.tf
+++ scaleway/versions.tf
@@ -1,8 +1,13 @@
 terraform {
   required_version = ">= 0.13"
   required_providers {
+    template = {
+      source  = "hashicorp/template"
+      version = "= 2.2.0"
+    }
     scaleway = {
-      source = "scaleway/scaleway"
+      source  = "scaleway/scaleway"
+      version = "= 2.55.0"
     }
   }
 }
