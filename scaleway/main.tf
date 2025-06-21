47b9a2c7bb1afc08d533d33a4f39f0763b318a2f
Hyacinthe Cartiaux <hyacinthe.cartiaux@free.fr>
2025-06-22 01:22:52 +0200

    [scaleway] cartiaux.net zone and records (mail hosted at galae.net)

--- scaleway/main.tf
+++ scaleway/main.tf
@@ -8,6 +8,156 @@ resource "scaleway_account_ssh_key" "main" {
 }
 
 ###
+# Cartiaux.net zone configuration
+###
+
+resource "scaleway_domain_record" "ns0-scw" {
+  dns_zone = "cartiaux.net"
+  type     = "NS"
+  data     = "ns0.dom.scw.cloud."
+  ttl      = 1800
+}
+resource "scaleway_domain_record" "ns1-scw" {
+  dns_zone = "cartiaux.net"
+  type     = "NS"
+  data     = "ns1.dom.scw.cloud."
+  ttl      = 1800
+}
+resource "scaleway_domain_record" "mx1" {
+  dns_zone = "cartiaux.net"
+  type     = "MX"
+  priority = 10
+  data     = "smtpin-01.galae.net."
+  ttl      = 10800
+}
+resource "scaleway_domain_record" "mx2" {
+  dns_zone = "cartiaux.net"
+  type     = "MX"
+  priority = 50
+  data     = "smtpin-02.galae.net."
+  ttl      = 10800
+}
+resource "scaleway_domain_record" "spf" {
+  dns_zone = "cartiaux.net"
+  type     = "TXT"
+  data     = "v=spf1 include:spf.galae.net ?all"
+  ttl      = 10800
+}
+resource "scaleway_domain_record" "dkim" {
+  dns_zone = "cartiaux.net"
+  name     = "dkim._domainkey"
+  type     = "TXT"
+  data     = "v=DKIM1;k=rsa;"
+  ttl      = 10800
+}
+resource "scaleway_domain_record" "dmarc" {
+  dns_zone = "cartiaux.net"
+  name     = "_dmarc"
+  type     = "TXT"
+  data     = "v=DMARC1; p=reject;"
+  ttl      = 10800
+}
+resource "scaleway_domain_record" "srv_autodiscover" {
+  dns_zone = "cartiaux.net"
+  name     = "_autodiscover._tcp"
+  type     = "SRV"
+  data     = "0 1 443 mail.galae.net."
+  ttl      = 10800
+}
+resource "scaleway_domain_record" "srv_caldavs" {
+  dns_zone = "cartiaux.net"
+  name     = "_caldavs._tcp"
+  type     = "SRV"
+  data     = "0 1 443 mail.galae.net."
+  ttl      = 10800
+}
+resource "scaleway_domain_record" "txt_caldavs" {
+  dns_zone = "cartiaux.net"
+  name     = "_caldavs._tcp"
+  type     = "TXT"
+  data     = "path=/SOGo/dav/"
+  ttl      = 10800
+}
+resource "scaleway_domain_record" "srv_carddavs" {
+  dns_zone = "cartiaux.net"
+  name     = "_carddavs._tcp"
+  type     = "SRV"
+  data     = "0 1 443 mail.galae.net."
+  ttl      = 10800
+}
+resource "scaleway_domain_record" "txt_carddavs" {
+  dns_zone = "cartiaux.net"
+  name     = "_carddavs._tcp"
+  type     = "TXT"
+  data     = "path=/SOGo/dav/"
+  ttl      = 10800
+}
+resource "scaleway_domain_record" "srv_imap" {
+  dns_zone = "cartiaux.net"
+  name     = "_imap._tcp"
+  type     = "SRV"
+  data     = "0 1 143 mail.galae.net."
+  ttl      = 10800
+}
+resource "scaleway_domain_record" "srv_imaps" {
+  dns_zone = "cartiaux.net"
+  name     = "_imaps._tcp"
+  type     = "SRV"
+  data     = "0 1 993 mail.galae.net."
+  ttl      = 10800
+}
+resource "scaleway_domain_record" "srv_pop3" {
+  dns_zone = "cartiaux.net"
+  name     = "_pop3._tcp"
+  type     = "SRV"
+  data     = "0 1 110 mail.galae.net."
+  ttl      = 10800
+}
+resource "scaleway_domain_record" "srv_pop3s" {
+  dns_zone = "cartiaux.net"
+  name     = "_pop3s._tcp"
+  type     = "SRV"
+  data     = "0 1 995 mail.galae.net."
+  ttl      = 10800
+}
+resource "scaleway_domain_record" "srv_sieve" {
+  dns_zone = "cartiaux.net"
+  name     = "_sieve._tcp"
+  type     = "SRV"
+  data     = "0 1 4190 mail.galae.net."
+  ttl      = 10800
+}
+resource "scaleway_domain_record" "srv_smtps" {
+  dns_zone = "cartiaux.net"
+  name     = "_smtps._tcp"
+  type     = "SRV"
+  data     = "0 1 465 mail.galae.net."
+  ttl      = 10800
+}
+resource "scaleway_domain_record" "srv_submission" {
+  dns_zone = "cartiaux.net"
+  name     = "_submission._tcp"
+  type     = "SRV"
+  data     = "0 1 587 mail.galae.net."
+  ttl      = 10800
+}
+resource "scaleway_domain_record" "cname_autoconfig" {
+  dns_zone = "cartiaux.net"
+  name     = "autoconfig"
+  type     = "CNAME"
+  data     = "mail.galae.net."
+  ttl      = 10800
+}
+resource "scaleway_domain_record" "cname_autodiscover" {
+  dns_zone = "cartiaux.net"
+  name     = "autodiscover"
+  type     = "CNAME"
+  data     = "mail.galae.net."
+  ttl      = 10800
+}
+
+
+###
 # Flap42.eu zone configuration
 ###
 
