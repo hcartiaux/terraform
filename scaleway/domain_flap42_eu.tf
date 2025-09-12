1c73066d5f634593a757d1fd0025adb87b8c9b3e
Hyacinthe Cartiaux <hyacinthe.cartiaux@free.fr>
2025-09-12 22:49:30 +0200

    [scaleway] flap42.eu - secondary name server migration

--- scaleway/domain_flap42_eu.tf
+++ scaleway/domain_flap42_eu.tf
@@ -16,11 +16,18 @@ resource "scaleway_domain_record" "ns1-glue" {
   data     = "2001:bc8:3feb:100::1"
   ttl      = 3600
 }
+resource "scaleway_domain_record" "ns2-glue-ip4" {
+  dns_zone = "flap42.eu"
+  name     = "ns2"
+  type     = "A"
+  data     = "129.151.240.9"
+  ttl      = 3600
+}
 resource "scaleway_domain_record" "ns2-glue" {
   dns_zone = "flap42.eu"
   name     = "ns2"
   type     = "AAAA"
-  data     = "2a13:e3c1:400e:1337::58"
+  data     = "2603:c026:c108:2101:0:f6f8:ce4:397b"
   ttl      = 3600
 }
 resource "scaleway_domain_record" "ns1" {
